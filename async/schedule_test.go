70a425e28270dd14a58ddc70ca3c28dbcacac0cb
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2019-12-06 20:32:09 +0100

    fix scheduler

--- async/schedule_test.go
+++ async/schedule_test.go
@@ -2,7 +2,31 @@ package async
 
 import "testing"
 
+import "time"
+
+import "gotest.tools/assert"
+
+type JobOne struct {
+	count int
+}
+
+func (j *JobOne) execute() {
+	for idx := 0; idx < 100; idx++ {
+		j.count++
+		time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func (j *JobOne) name() string {
+	return "JobOne"
+}
+
 func TestSchedulerInitDeinit(t *testing.T) {
-	Init(1)
+	job := JobOne{count: 0}
+	Init(1, 2)
+	TryEnqueue(&job)
+	//give time to scheduler to execute the job
+	time.Sleep(100 * time.Millisecond)
 	Deinit()
+	assert.Assert(t, job.count == 100)
 }
--- async/scheduler.go
+++ async/scheduler.go
@@ -1,40 +1,53 @@
 package async
 
-import "log"
-
-import "context"
+import (
+	"context"
+	"log"
+	"sync"
+)
 
 var runnableChan chan Runnable
 var ctx context.Context
 var cancel context.CancelFunc
+var wg sync.WaitGroup
 
 func execute(ctx context.Context, runnableChan <-chan Runnable) {
+	log.Println("Entering scheduler execute")
+	defer wg.Done()
 	for {
 		select {
 		case <-ctx.Done():
-			log.Println("Exit runnable scheduler")
+			log.Println("Leaving runnable scheduler")
 			return
 
 		case r := <-runnableChan:
 			log.Printf("Executing new job %s\n", r.name())
 			r.execute()
 			if ctx.Err() != nil {
+				log.Println("Scheduler error -> leaving runnable scheduler")
 				return
 			}
 		}
 	}
+
 }
 
 // Init ...
-func Init(queueSize int) {
+func Init(queueSize int, workerCount int) {
 	runnableChan = make(chan Runnable, queueSize)
 	ctx, cancel = context.WithCancel(context.Background())
-	go execute(ctx, runnableChan)
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go execute(ctx, runnableChan)
+	}
+
 }
 
 // Deinit ...
 func Deinit() {
-	close(runnableChan)
+	//call cancel on context
+	cancel()
+	wg.Wait()
 }
 
 // TryEnqueue impl.
