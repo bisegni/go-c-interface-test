39a2b0b3edd862cff341c8faafd08d87f5707b94
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2019-12-08 15:35:01 +0100

    ported fix from master

--- adapter/downloader.go
+++ adapter/downloader.go
@@ -1,3 +1,11 @@
-package aws
+package adapter
 
+// Dowloader interface
+type Dowloader interface {
+	Download(c *Config, uri string, destPath string) (err error)
+}
 
+// Uploader interface
+type Uploader interface {
+	// Download(c *Config, uri string, destPath string) (err error)
+}
